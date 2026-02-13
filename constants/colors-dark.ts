a112ca0c1061aaa2272f0198a4232884ef6327f5
Ragoub Anes <152559421+its-Xero@users.noreply.github.com>
2026-02-13 14:19:05 +0100

    Add files via upload

---
+++ constants/colors-dark.ts
@@ -0,0 +1,13 @@
+// 🎨 COULEURS PEXELIS - DESIGN EXACT
+export const COLORS = {
+  background: '#04031D',//Gris clair (fond de l'app)
+  card: '#FFFFFF',           // Blanc (fond des cartes)
+  border: '#D8D8D8',         // Bleu clair (bordures)
+  primary: '#1E4E86',        // Bleu foncé (icônes, textes actifs)
+  secondary: '#4C5EA2',     // Bleu violet (secondaire)
+  accent: '#479DFF',        // Bleu vif (liens, boutons)
+  textPrimary: '#262626',   // Gris très foncé (titres)
+  textSecondary: '#1E1A3B', // Violet foncé (textes secondaires)
+  markDoneBg: '#D8D8D8',    // Gris clair (fond bouton)
+} as const ;
+export default COLORS;
---
+++ constants/colors-light.ts
@@ -0,0 +1,13 @@
+// 🎨 COULEURS MODE LIGHT - DESIGN EXACT
+export const colors = {
+  background: '#90BBEA',     // Gris clair - fond page
+  card: '#616FA6',           // Blanc - fond tableau
+  border: '#000000',         // Bleu clair - bordures
+  primary: '#1E4E86',        // Bleu foncé - status
+  secondary: '#1E4E86',      // Bleu violet - en-tête
+  textPrimary: '#262626',    // Gris foncé - titres
+  textSecondary: '#D8D8D8',  // Noir - texte normal
+  settings: '#4C5EA2',       // Bleu violet - bouton settings
+} as const;
+
+export default colors;
